68b49f015ac1176988c4578050a049e006bbabd8
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-06-08 21:01:49 +0200

    displaying data from local json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
 import './App.css';
+import { useEffect, useState } from "react";
+import Json from './data.json';
 
 function App() {
-  return (
-    <div className="">
+  //console.log(Data);
 
+  let data = Json.results;
+
+  /*   const [results, setResults] = useState([])
+  
+    function fetchData() {
+  
+      //const data = './data.json'
+      fetch(Data
+        , {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          }
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => setResults(data.results))
+    } */
+
+  /*   function fetchData() {
+  
+      fetch("https://swapi.dev/api/people/")
+        .then(res => res.json())
+        .then(data => setResults(data.results))
+    }
+   */
+  /*   useEffect(() => {
+      fetchData();
+    }, []); */
+
+
+  return (
+    <div>
+      {data.map((item, index) => {
+        return (
+          <div key={index}>
+            <h1>{item.name}</h1>
+
+          </div>
+        )
+
+      })}
     </div>
+
+
+
+    /*     < div >
+          {
+            Data.map((item, index) => {
+              return (
+                <div key={index}>
+                  <h1>{item.username}</h1>
+                </div>
+              )
+    
+            })
+          }
+        </div > */
   );
 }
 
