4049b91902049bafd91f4dcedc4c2a4207e6d767
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-06-13 21:45:31 +0200

    displaying data

--- src/App.js
+++ src/App.js
@@ -1,51 +1,63 @@
 import './App.css';
 import { useEffect, useState } from "react";
 import Json from './data.json';
-import Card from './components/Card';
+import Card from './components/MyAccordion';
+import axios from 'axios'
 
 function App() {
   //console.log(Data);
 
-  let data = Json.results;
+  //let data = Json.results;
+  let data2 = Json;
 
-  /*   const [results, setResults] = useState([])
-  
-    function fetchData() {
-  
-      //const data = './data.json'
-      fetch(Data
-        , {
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          }
-        }
-      )
-        .then((res) => res.json())
-        .then((data) => setResults(data.results))
-    } */
+  const [apiData, setApiData] = useState(null)
 
-  /*   function fetchData() {
-  
-      fetch("https://swapi.dev/api/people/")
-        .then(res => res.json())
-        .then(data => setResults(data.results))
-    }
-   */
-  /*   useEffect(() => {
-      fetchData();
-    }, []); */
+  function fetchData() {
 
+    fetch("http://swapi.dev/api/people")
+      .then(res => res.json())
+      .then(data => setApiData(data))
+
+  }
+
+  const getPage = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setApiData(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="container m-3">
-      {data.map((item, index) => {
+      {apiData && apiData.results.map((item, index) => {
         return <Card
           key={index}
           fullName={item.name}
+          gender={item.gender}
+          height={item.height}
+          birth_year={item.birth_year}
         ></Card>
       })}
 
+      {apiData && (apiData.next || apiData.previous) ? (
+        <div>
+          <button
+            disabled={!apiData.previous}
+            onClick={() => getPage(apiData.previous)}
+          >
+            Previous page
+          </button>
+          <button
+            disabled={!apiData.next}
+            onClick={() => getPage(apiData.next)}
+          >
+            Next page
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
